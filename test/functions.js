6b246329e24c4ba40b448cbbc4245e134ab55d6c
dmosley02 <donald_mosley@hotmail.com>
2025-12-11 14:23:36 -0600

    my test files so i didnt mess anything up

---
+++ test/functions.js
@@ -0,0 +1,23 @@
+//variables
+let clickCount = 0;
+let multiplyer = 1;
+const upgradeMultiplier = 2;
+
+const cookieButton = document.getElementById("clicker");
+const counterDisplay = document.getElementById("counterDisplay");
+const multiplyerDisplay = document.getElementById("multiplyerDisplay");
+const upgradeButton = document.getElementById("upgradeButton");
+
+//clicker button
+cookieButton.addEventListener('click', function(e)
+    {
+        clickCount += multiplyer;
+        counterDisplay.textContent = clickCount;
+    });
+
+//upgrgade button
+upgradeButton.addEventListener('click', function(e)
+    {
+        multiplyer += upgradeMultiplier;
+        multiplyerDisplay.textContent = + multiplyer;
+    });
