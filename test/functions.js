a1aa3ca7c30d7ab69051b40177d8bb49768f8043
dmosley02 <donald_mosley@hotmail.com>
2025-12-11 18:10:17 -0600

    auto clicker added

--- test/functions.js
+++ test/functions.js
@@ -1,23 +1,37 @@
 //variables
 let clickCount = 0;
 let multiplyer = 1;
+let autoClicker = 0;
 const upgradeMultiplier = 2;
 
 const cookieButton = document.getElementById("clicker");
 const counterDisplay = document.getElementById("counterDisplay");
 const multiplyerDisplay = document.getElementById("multiplyerDisplay");
 const upgradeButton = document.getElementById("upgradeButton");
+const autoClickerButton = document.getElementById("autoClickerButton");
 
 //clicker button
 cookieButton.addEventListener('click', function(e)
     {
         clickCount += multiplyer;
-        counterDisplay.textContent = clickCount;
+        counterDisplay.textContent = clickCount + autoClicker;
     });
 
 //upgrgade button
 upgradeButton.addEventListener('click', function(e)
     {
-        multiplyer += upgradeMultiplier;
+        multiplyer *= upgradeMultiplier;
         multiplyerDisplay.textContent = + multiplyer;
     });
+
+//auto clicker button
+function auto() {
+    autoClicker += multiplyer;
+    counterDisplay.textContent = clickCount + autoClicker;
+}
+autoClickerButton.addEventListener('click', function(e)
+    {
+        intervalId = setInterval(auto, 100);
+        autoClickerButton.style.visibility = 'hidden';
+    }
+);
