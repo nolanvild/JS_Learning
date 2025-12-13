d8d0c7967d46dcb5df9ef0786aadab1f89346b50
dmosley02 <donald_mosley@hotmail.com>
2025-12-12 18:53:09 -0600

    fixing upgrade buttons

--- test/functions.js
+++ test/functions.js
@@ -1,3 +1,8 @@
+// balance cost of upgrades scaling from 100 clicks for first multiplyer and scale 1.5
+// times the cost for each upgrade after, auto clicker worth 10000, make it click 10 times per second
+
+//add alert when you arent able to
+
 //variables
 let clickCount = 0;
 let multiplyer = 1;
@@ -9,37 +14,46 @@ const counterDisplay = document.getElementById("counterDisplay");
 const multiplyerDisplay = document.getElementById("multiplyerDisplay");
 const upgradeButton = document.getElementById("upgradeButton");
 const autoClickerButton = document.getElementById("autoClickerButton");
+const upgradeCostDisplay = document.getElementById("upgradeCostDisplay");
+const autoCostDisplay = document.getElementById("autoCostDisplay");
 
 //clicker button
-cookieButton.addEventListener('click', function(e)
-    {
-        clickCount += multiplyer;
-        counterDisplay.textContent = clickCount + autoClicker;
-    });
+cookieButton.addEventListener("click", function (e) {
+  clickCount += multiplyer;
+  counterDisplay.textContent = clickCount + autoClicker;
+});
 
 //upgrade button
-upgradeButton.addEventListener('click', function(e)
-    {
-        multiplyer *= upgradeMultiplier;
-        multiplyerDisplay.textContent = + multiplyer
-        clickCount -= upgradeButtonCost;
-        upgradeButton.style.visibility = 'hidden';
-        upgradeButtonCost *= upgradeButtonScaling
-    });
+upgradeButton.addEventListener("click", function (e) {
+  if (clickCount - upgradeButtonCost >= 0) {
+    multiplyer *= upgradeMultiplier;
+    multiplyerDisplay.textContent = +multiplyer;
+    clickCount -= upgradeButtonCost;
+    upgradeButtonCost *= upgradeButtonScaling;
+    upgradeCostDisplay.textContent = upgradeButtonCost;
+    counterDisplay.textContent = clickCount;
+  } else {
+  }
+});
 
 //auto clicker button
 function auto() {
-    autoClicker += multiplyer;
-    counterDisplay.textContent = clickCount + autoClicker;
+  autoClicker += multiplyer;
+  counterDisplay.textContent = clickCount + autoClicker;
 }
-autoClickerButton.addEventListener('click', function(e)
-    {
-        let intervalId = setInterval(auto, 1000);
-        clickCount = clickCount - autoClickerCost;
-        autoClickerButton.style.visibility = 'hidden';
-        autoClickerCost *= autoClickerScaling
-    }
-);
+autoClickerButton.addEventListener("click", function (e) {
+  if (clickCount - autoClickerCost >= 0) {
+    let intervalId = setInterval(auto, 100);
+    clickCount = clickCount - autoClickerCost;
+    autoClickerButton.style.visibility = "hidden";
+    autoClickerCost *= autoClickerScaling;
+    counterDisplay.textContent = clickCount;
+  } else {
+    Error;
+  }
+});
+
+//not enough funds alert
 
 // upgrade visibility
 let autoClickerCost = 10;
@@ -47,15 +61,13 @@ let upgradeButtonCost = 10;
 const autoClickerScaling = 2;
 const upgradeButtonScaling = 2;
 
-autoClickerButton.style.visibility = 'hidden';
-upgradeButton.style.visibility = "hidden";
-const upgradeCheck = setInterval(checkForUpgrades, 500);
+const upgradeCheck = setInterval(checkForUpgrades, 100);
 
-function checkForUpgrades(){
-    if (clickCount >= autoClickerCost) {
-        autoClickerButton.style.visibility = 'visible';
-        upgradeButton.style.visibility = 'visible';
-        }
+function checkForUpgrades() {
+  if (clickCount >= autoClickerCost) {
+    autoClickerButton.style.visibility = "visible";
+    upgradeButton.style.visibility = "visible";
+  }
 }
 
-export  {clickCount}
+export { clickCount };
